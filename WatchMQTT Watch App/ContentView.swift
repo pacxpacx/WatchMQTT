7315a82499c41c1cedb7d56e98cfabc45c52fd15
MIA <bo110crestron@gmail.com>
2025-05-27 13:52:10 +0200

    Initial Commit

---
+++ WatchMQTT Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchMQTT Watch App
+//
+//  Created by Pavel Cintins on 27/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WatchMQTT Watch App/WatchMQTTApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchMQTTApp.swift
+//  WatchMQTT Watch App
+//
+//  Created by Pavel Cintins on 27/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct WatchMQTT_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
