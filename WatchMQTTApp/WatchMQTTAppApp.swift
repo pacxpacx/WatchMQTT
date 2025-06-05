a23e5bd90366ceb2417701e28fa707686d02bfcc
MIA <bo110crestron@gmail.com>
2025-06-05 17:58:17 +0200

    Add Info.plist and initial source files

---
+++ WatchMQTTApp/WatchMQTTAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchMQTTAppApp.swift
+//  WatchMQTTApp
+//
+//  Created by Pavel Cintins on 27/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct WatchMQTTAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
